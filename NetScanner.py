bd9676405eacde42bf84936864ac97488a54a6a5
umitdogan <umit9551@gmail.com>
2022-07-23 21:14:29 +0300

    end refactor NetScanner

--- NetScanner.py
+++ NetScanner.py
@@ -22,8 +22,8 @@ def get_user_input():
 def scan_my_network(ip):
     if not ip:
         ifconfig = sp.check_output(["ifconfig","-v","wlan0"]);
-        ip_address = re.search("\w\w\w\w \d.\d.\d.\d.\d.\d.",str(ifconfig));
-        ip = ip_address.group(0)[0] + "/24";
+        ip_address = re.search("\d.\d.\d.\d.\d.\d\d",str(ifconfig));
+        ip = ip_address.group(0)+ "/24";
     #"192.168.1.43/24"
     arp_request_packet = scapy.ARP(pdst=str(ip))
     broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
